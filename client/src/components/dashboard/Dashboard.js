9abceb5a3b9b4344471e452d46c70d25a3fc86d6
Abdullah <abdullahbukharii10@gmail.com>
2021-12-30 15:49:42 +0530

    Task 1 done ig...

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 
+
 class Dashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
@@ -13,25 +14,21 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div style={{ height: "25vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="landing-copy col s12 center-align">
             <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
+              <b>Task 2</b> 
             </h4>
             <button
               style={{
-                width: "150px",
+                width: "100px",
                 borderRadius: "3px",
-                letterSpacing: "1.5px",
+                letterSpacing: "1px",
                 marginTop: "1rem"
               }}
               onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              className="btn btn-medium waves-effect waves-teal-text"
             >
               Logout
             </button>
